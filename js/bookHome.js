00fff6df8c65e75da443a5a9e11fa9f7477cb591
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2021-03-16 17:39:34 +0530

    Added:Populated address book list from Local Storage

--- js/bookHome.js
+++ js/bookHome.js
@@ -1,50 +1,37 @@
+let personList;
 window.addEventListener('DOMContentLoaded', (event)=>{
+  personList = getaddressBookDataFromStorage();
+  document.querySelector(".person-count").textContent = personList.length;
   createInnerHtml();
 });
 
+const getaddressBookDataFromStorage = () => { 
+  return localStorage.getItem('AddressBookList') ?
+             JSON.parse(localStorage.getItem('AddressBookList')) : [];
+}
+
 const createInnerHtml = () => { 
+  if(personList.length == 0) return;
   const headerHtml = "<tr><th>Full Name</th><th>Phone Number</th><th>Address</th><th>City</th>"+
                       "<th>State</th><th>Zip</th><th>Actions</th></tr>";
   let innerHtml = `${headerHtml}`;
-  let personBookList = createAddressBookJSON();
-  for (const personBookData of personBookList) {
+  for (const personBookData of personList) {
         innerHtml = `${innerHtml}                   
     <tr>
       <td>${personBookData._name}</td>
-      <td>${personBookData._phoneNumber}</td>
+      <td>${personBookData._number}</td>
       <td>${personBookData._address}</td>
       <td>${personBookData._city}</td>
       <td>${personBookData._state}</td>
       <td>${personBookData._zip}</td>
       <td>
-        <img name="1" onclick="remove(this)" alt="delete"
+        <img name="${personBookData._name}" onclick="remove(this)" alt="delete"
           src="../assets/icon/delete-black-18dp.svg">
-        <img name="1"  alt="edit" onclick="update(this)"
+        <img name="${personBookData._name}"  alt="edit" onclick="update(this)"
           src="../assets/icon/create-black-18dp.svg">
       </td>
     </tr>
     `;
   }
   document.querySelector('#table-display').innerHTML = innerHtml;
-}
-const createAddressBookJSON = () => {
-  let personBookListLocal = [
-      {
-          _name: 'Akhil',
-          _phoneNumber: '7895632142',
-          _address:'ABC chowk,pune',
-          _city: 'Pune',
-          _state: 'Maharashtra',
-          _zip: '411006',
-      },
-      {
-          _name: 'Pandurang',
-          _phoneNumber: '8895632142',
-          _address:'police colony,Thane',
-          _city: 'Thane',
-          _state: 'Maharashtra',
-          _zip: '498566',
-      }
-  ];
-  return personBookListLocal;
 }
